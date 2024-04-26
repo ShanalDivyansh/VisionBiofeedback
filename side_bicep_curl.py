0d3c695ae48f7561834f7f81c14183e8d5d69616
SurajSanjaySingh <singhsurajs1311@gmail.com>
2024-04-26 16:49:05 -0400

    checked sbc file

--- side_bicep_curl.py
+++ side_bicep_curl.py
@@ -18,46 +18,46 @@ class side_bicep_curl(object):
             "wrist": time.time(),
             "elbow": time.time()
         }
-    #     self.display_colors = ""
-    # def sbc(self,display_colors,midpointX,midpointY):
-    #     current_time = time.time()
-    #     self.display_colors = display_colors
-    #     print(self.display_colors)
-    #     if self.display_colors == "#32ba19":
-    #         self.last_keypoint_time["shoulder"] = time.time()
+        self.display_colors = ""
+    def sbc(self,display_colors,midpointX,midpointY):
+        current_time = time.time()
+        self.display_colors = display_colors
+        print(self.display_colors)
+        if self.display_colors == "#32ba19":
+            self.last_keypoint_time["shoulder"] = time.time()
 
-    #     if self.display_colors == "#000c7c":
-    #         self.last_keypoint_time["wrist"] = time.time()
+        if self.display_colors == "#000c7c":
+            self.last_keypoint_time["wrist"] = time.time()
 
-    #     if self.display_colors == "#005fa3":
-    #         self.last_keypoint_time["elbow"] = time.time()
+        if self.display_colors == "#005fa3":
+            self.last_keypoint_time["elbow"] = time.time()
 
-    #     if current_time - self.last_keypoint_time["shoulder"] > 5  or current_time - self.last_keypoint_time["wrist"] > 5 or current_time - self.last_keypoint_time["elbow"] > 5:
-    #         raise Exception
+        if current_time - self.last_keypoint_time["shoulder"] > 5  or current_time - self.last_keypoint_time["wrist"] > 5 or current_time - self.last_keypoint_time["elbow"] > 5:
+            raise Exception
 
-    #     if self.display_colors == "#32ba19":
-    #         self.xShoulder,self.yShoulder = midpointX,midpointY
-    #     #     # print( self.xShoulder, self.yShoulder,"**SHOULDER**")
-    #     if self.display_colors == "#005fa3":
-    #         self.xElbow, self.yElbow = midpointX,midpointY
-    #     #     # print( self.xElbow, self.yElbow,"**ELBOW**")
-    #     if self.display_colors == "#000c7c":
-    #         self.xWrist, self.yWrist = midpointX,midpointY
-    #     #     # print( self.xWrist, self.yWrist,"**Wrist**")                                      
+        if self.display_colors == "#32ba19":
+            self.xShoulder,self.yShoulder = midpointX,midpointY
+        #     # print( self.xShoulder, self.yShoulder,"**SHOULDER**")
+        if self.display_colors == "#005fa3":
+            self.xElbow, self.yElbow = midpointX,midpointY
+        #     # print( self.xElbow, self.yElbow,"**ELBOW**")
+        if self.display_colors == "#000c7c":
+            self.xWrist, self.yWrist = midpointX,midpointY
+        #     # print( self.xWrist, self.yWrist,"**Wrist**")                                      
 
-    #     if abs( self.xShoulder -  self.xElbow)>8:
-    #         print("Shoulder and elbow not aligned")
-    #     # print(self.xShoulder-self.xWrist)
-    #     self.finalPoint = self.yShoulder+47
-    #     ## Start position
-    #     if not self.startPoint:
-    #         print("SHOULDER WRIST AND ELBOW ARE ALIGNED")
-    #         if 40.0 <= abs(self.xShoulder - self.xWrist) <= 60.0:
-    #             self.startPoint = self.yWrist
+        if abs( self.xShoulder -  self.xElbow)>8:
+            print("Shoulder and elbow not aligned")
+        # print(self.xShoulder-self.xWrist)
+        self.finalPoint = self.yShoulder+47
+        ## Start position
+        if not self.startPoint:
+            print("SHOULDER WRIST AND ELBOW ARE ALIGNED")
+            if 40.0 <= abs(self.xShoulder - self.xWrist) <= 60.0:
+                self.startPoint = self.yWrist
                 
-    #     print( "93","PROGRESSSSSSSSSS",self.finalPoint,self.startPoint,self.yWrist)
+        print( "93","PROGRESSSSSSSSSS",self.finalPoint,self.startPoint,self.yWrist)
 
-    #     self.progress = ( (self.startPoint-self.yWrist)/(self.startPoint-self.finalPoint))*100
+        self.progress = ( (self.startPoint-self.yWrist)/(self.startPoint-self.finalPoint))*100
                                     
         print(self.counted)
         if self.startPoint and not self.counted:
